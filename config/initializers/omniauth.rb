e0d4b8216716911c33b8c6e72f8f76c8f5c06ffb
Sasha Nazar <nazar.sasha@gmail.com>
2016-02-24 15:36:05 +0200

    added twit keys

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
+  provider :twitter, "vSapJky5iuX6RRU8Kn08eV5MF", "IkJ87aYqFIlrmL5GDSUBdvEFnLujA5a8BAHWQrPpITtnysOEtP" #ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
 
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
            scope: 'public_profile', info_fields: 'id,name,link'
